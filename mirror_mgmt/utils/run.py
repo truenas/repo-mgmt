4e41fa904c17795f3e864466069ca3d08100dc49
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-11-12 20:20:40 +0500

    Add run utils file

---
+++ mirror_mgmt/utils/run.py
@@ -0,0 +1,41 @@
+import logging
+import os
+import subprocess
+
+from mirror_mgmt.exceptions import CallError
+
+
+logger = logging.getLogger(__name__)
+
+
+def run(*args, **kwargs):
+    if isinstance(args[0], list):
+        args = tuple(args[0])
+    kwargs.setdefault('stdout', subprocess.PIPE)
+    kwargs.setdefault('stderr', subprocess.PIPE)
+    exception_message = kwargs.pop('exception_msg', None)
+    check = kwargs.pop('check', True)
+    shell = kwargs.pop('shell', False)
+    log = kwargs.pop('log', True)
+    env = kwargs.pop('env', None) or os.environ
+    if log:
+        kwargs['stderr'] = subprocess.STDOUT
+
+    proc = subprocess.Popen(
+        args, stdout=kwargs['stdout'], stderr=kwargs['stderr'], shell=shell, env=env, encoding='utf8', errors='ignore'
+    )
+    if log:
+        for line in map(str.rstrip, iter(proc.stdout.readline, '')):
+            logger.debug(line)
+
+    stdout, stderr = proc.communicate()
+
+    cp = subprocess.CompletedProcess(args, proc.returncode, stdout=stdout, stderr=stderr)
+    if check:
+        error_str = exception_message or stderr or ''
+        if cp.returncode:
+            raise CallError(
+                f'Command {" ".join(args) if isinstance(args, list) else args!r} returned exit code '
+                f'{cp.returncode}' + (f' ({error_str})' if error_str else '')
+            )
+    return cp
