e597cabf10d5e052b71b34468e609d38834f72a5
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-11-14 13:59:50 +0500

    Add endpoints to create/update mirror

--- mirror_mgmt/aptly/mirror.py
+++ mirror_mgmt/aptly/mirror.py
@@ -6,12 +6,32 @@ def run(command, **kwargs):
 
 
 class Mirror:
-    def __init__(self, name):
+    def __init__(self, name, **kwargs):
         self.name = name
+        self.repository = kwargs.get('repository')
+        self.distribution = kwargs.get('distribution')
+        self.component = kwargs.get('component')
 
     @property
     def exists(self):
         return run(['show', self.name], check=False).returncode == 0
+
+    def ensure_exists(self, repository=None, distribution=None, component=None, filters=None, extra_options=None):
+        if not self.exists:
+            self.create(
+                repository or self.repository, distribution or self.distribution, component or self.component,
+                filters, extra_options,
+            )
+
+    def create(self, repository, distribution, component, filters=None, extra_options=None):
+        return run(list(filter(
+            bool, ['create'] + (extra_options or []) + ([f'-filter={filters}'] if filters else []) + [
+                self.name, repository, distribution, component,
+            ]
+        )))
+
+    def update(self):
+        run(['update', self.name])
 
 
 def list_mirrors():
