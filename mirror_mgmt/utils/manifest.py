e107d7cf4e9b9ffd3fba3f269812242e77c0c0dc
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-11-17 20:14:53 +0500

    Validate provided manifest

--- mirror_mgmt/utils/manifest.py
+++ mirror_mgmt/utils/manifest.py
@@ -1,9 +1,47 @@
 import functools
+import jsonschema
 import yaml
 
 from mirror_mgmt.exceptions import CallError, MissingManifest
 
 from .paths import MANIFEST_PATH
+
+
+MANIFEST_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'release': {'type': 'string'},
+        'gpg_key': {'type': 'string'},
+        'publish_prefix_default': {'type': 'string'},
+        'mirror_suffix': {'type': 'string'},
+        'mirrors': {
+            'type': 'array',
+            'items': [{
+                'type': 'object',
+                'properties': {
+                    'name': {'type': 'string'},
+                    'url': {'type': 'string'},
+                    'distribution': {'type': 'string'},
+                    'publish_prefix_override': {'type': 'string'},
+                    'component': {'type': 'string'},
+                    'filter': {'type': 'string'},
+                    'extra_options': {
+                        'type': 'array',
+                        'items': [{'type': 'string'}]
+                    },
+                },
+                'required': ['name', 'url', 'distribution', 'component']
+            }],
+        },
+    },
+    'required': [
+        'release',
+        'gpg_key',
+        'publish_prefix_default',
+        'mirror_suffix',
+        'mirrors',
+    ],
+}
 
 
 def get_manifest_str() -> str:
@@ -16,12 +54,14 @@ def get_manifest_str() -> str:
 
 @functools.cache
 def get_manifest() -> dict:
-    # TODO: Validate provided manifest
     try:
         manifest = yaml.safe_load(get_manifest_str())
+        jsonschema.validate(manifest, MANIFEST_SCHEMA)
         return manifest
     except yaml.YAMLError:
         raise CallError('Provided manifest has invalid format')
+    except jsonschema.ValidationError as e:
+        raise CallError(f'Provided manifest is invalid: {e}')
 
 
 def get_release_code_name() -> str:
