cfd5adf3fe9a330b2930821ad6d17c70190a4857
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-11-17 17:42:04 +0500

    Allow specifying prefix in manifest for publishing snapshot

--- mirror_mgmt/aptly/snapshot.py
+++ mirror_mgmt/aptly/snapshot.py
@@ -7,6 +7,10 @@ from .run import aptly_run
 P = ParamSpec('P')
 
 
+def get_endpoint(prefix: str) -> str:
+    return f'filesystem:truenas:{prefix}'
+
+
 def run(command: list, **kwargs: P.kwargs) -> subprocess.CompletedProcess:
     return aptly_run(['snapshot'] + command, **kwargs)
 
@@ -22,13 +26,14 @@ class Snapshot:
     def create(self, mirror) -> None:
         run(['create', self.name, 'from', 'mirror', mirror])
 
-    def publish(self, distribution: str, endpoint: str, gpg_key: str) -> None:
+    def publish(self, distribution: str, prefix: str, gpg_key: str) -> None:
         aptly_run([
-            'publish', 'snapshot', f'-distribution="{distribution}"', f'-gpg-key={gpg_key}', self.name, endpoint,
+            'publish', 'snapshot', f'-distribution="{distribution}"', f'-gpg-key={gpg_key}',
+            self.name, get_endpoint(prefix),
         ])
 
-    def drop_published_snapshot(self, distribution: str, endpoint: str) -> None:
-        aptly_run(['publish', 'drop', distribution, endpoint], check=False)
+    def drop_published_snapshot(self, distribution: str, prefix: str) -> None:
+        aptly_run(['publish', 'drop', distribution, get_endpoint(prefix)], check=False)
 
 
 def list_snapshots():
