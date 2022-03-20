16a87e4f17976040da76342693fe1faa625c6ad4
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-03-21 04:13:05 +0500

    Clean usages of repo from clean command

--- mirror_mgmt/clean.py
+++ mirror_mgmt/clean.py
@@ -1,30 +1,21 @@
 import logging
 
 from .aptly.resource import clean_database
-from .list import get_manifest_mirrors, get_manifest_repositories
+from .list import get_manifest_mirrors
 
 
 logger = logging.getLogger(__name__)
 
 
-def common_drop(repo_mirrors: list, object_type: str) -> None:
-    for repo_mirror in repo_mirrors:
-        if repo_mirror.exists:
-            logger.info('Removing %r %s', repo_mirror.name, object_type)
-            repo_mirror.drop()
+def clean_mirrors() -> None:
+    logger.debug('Cleaning mirrors')
+    for mirror in get_manifest_mirrors():
+        if mirror.exists:
+            logger.info('Removing %r mirror', mirror.name)
+            mirror.drop()
         else:
-            logger.info('%r %s does not exist, skipping dropping it', repo_mirror.name, object_type)
+            logger.info('%r mirror does not exist, skipping dropping it', mirror.name)
 
     logger.info('Cleaning aptly database')
     clean_database()
     logger.info('Successfully cleaned aptly database')
-
-
-def clean_mirrors() -> None:
-    logger.debug('Cleaning mirrors')
-    common_drop(get_manifest_mirrors(), 'mirror')
-
-
-def clean_repositories() -> None:
-    logger.debug('Cleaning repositories')
-    common_drop(get_manifest_repositories(), 'repository')
